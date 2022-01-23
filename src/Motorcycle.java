48c531579afcd06de2f88989e1927fc73dcb64bc
Scleepy <danielyohanes03@gmail.com>
2022-01-23 23:22:22 +0700

    first commit

---
+++ src/Motorcycle.java
@@ -0,0 +1,30 @@
+public class Motorcycle extends Vehicle {
+
+    private int helmetCount;
+
+    public Motorcycle(String brand, String name, String license, int topSpeed, int gasCapacity, int wheels, String type, int helmetCount){
+        super(brand, name, license, type, topSpeed, gasCapacity, wheels);
+        this.helmetCount = helmetCount;
+    }
+
+    @Override
+    public void runFeature(){
+        String message = getName() + " is standing!";
+
+        for(int i = 0; i < message.length(); i++){
+
+            try{
+                Thread.sleep(100);
+                System.out.print(message.charAt(i));
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            
+        }
+        // System.out.println(getName() + " is standing!");
+    }
+
+    public int getHelmetCount(){
+        return helmetCount;
+    }
+}
